640517912096bb0bd7d47b606f834e16990e7949
saleh <saleh.nazih.dev@gmail.com>
2019-11-08 09:57:25 -0300

    Communication Order

--- project/api/orders/views.py
+++ project/api/orders/views.py
@@ -5,3 +5,16 @@ import os
 
 orders_blueprint = Blueprint('orders', __name__)
 CORS(orders_blueprint)
+
+@orders_blueprint.route('/api/order-bag', methods=['POST'])
+def order_bag():
+    data = request.get_json()
+
+    # items = data.pop('items')
+
+    # # print(data)
+    # print(data)
+    response = requests.post('%s/create_order/' % os.getenv('ORDERS_PATH'), json=data)    
+    # response = request.post('http://0.0.0.0:8002/create_item/' % os.getenv('ORDERS_PATH'), json=items)
+
+    return jsonify(response.json()), response.status_code
