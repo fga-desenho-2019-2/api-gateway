0c533bd4068dae5b98e027b4cdc9a9be45aaa546
Sara Silva <saracsas2@gmail.com>
2019-11-17 15:48:21 -0300

    Add endpoints for categories and some fixs
    Co-authored-by: Pedro Rodrigues <peddroprp@gmail.com>
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>

--- project/api/restaurant/views.py
+++ project/api/restaurant/views.py
@@ -13,16 +13,16 @@ def get_all_restaurant():
     if response.status_code == 200:
         return jsonify(response.json()), response.status_code
 
-    return 400
+    return response.status_code
 
 @restaurant_blueprint.route('/api/restaurant/<int:pk>', methods=['GET'])
-def get_one_restaurant():
-    response = requests.get('%s/api/restaurant/<int:pk>' % os.getenv('RESTAURANT_PATH')) 
+def get_one_restaurant(pk):
+    response = requests.get(f'%s/api/restaurant/{pk}' % os.getenv('RESTAURANT_PATH')) 
     
     if response.status_code == 200:
         return jsonify(response.json()), response.status_code
 
-    return 400
+    return response.status_code
 
 @restaurant_blueprint.route('/api/items', methods=['GET'])
 def get_all_items():
@@ -31,13 +31,32 @@ def get_all_items():
     if response.status_code == 200:
         return jsonify(response.json()), response.status_code
 
-    return 400
+    return response.status_code
 
 @restaurant_blueprint.route('/api/item/<int:pk>', methods=['GET'])
-def get_one_item():
-    response_item = requests.get('%s/api/item/<int:pk>' % os.getenv('RESTAURANT_PATH')) 
+def get_one_item(pk):
+    response = requests.get(f'%s/api/item/{pk}' % os.getenv('RESTAURANT_PATH')) 
     
     if response.status_code == 200:
         return jsonify(response.json()), response.status_code
 
-    return 400
+    return response.status_code
+
+
+@restaurant_blueprint.route('/api/item/category', methods=['GET'])
+def get_all_item_categories():
+    response = requests.get('%s/api/item-category' % os.getenv('RESTAURANT_PATH'))
+
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return response.status_code
+
+@restaurant_blueprint.route('/api/restaurant/category', methods=['GET'])
+def get_all_restaurant_categories():
+    response = requests.get('%s/api/restaurant-category' % os.getenv('RESTAURANT_PATH'))
+
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return response.status_code
