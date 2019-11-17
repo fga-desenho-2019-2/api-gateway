d600500e546821f2d444fe50e2465b7505549c29
Elias Bernardo <ebmm01@gmail.com>
2019-11-17 20:05:14 -0300

    Fixing user service integrations and urls

--- project/api/users/views.py
+++ project/api/users/views.py
@@ -12,18 +12,20 @@ CORS(users_blueprint)
 def create_user():
     data = request.get_json()
     response = requests.post("%s/api/user/" % os.getenv('USERS_PATH'), json=data)
+    print(response.text)
     return jsonify(response.json()), response.status_code
 
-@users_blueprint.route('/user/<string:cpf>', methods=['GET'])
+@users_blueprint.route('/get_user/<string:cpf>', methods=['GET'])
 @needs_auth
 def get_user(cpf):
     response = requests.get("%s/api/get_user/%s" % (os.getenv('USERS_PATH'), cpf))
     return jsonify(response.json()), response.status_code
 
-@users_blueprint.route('/user/<string:cpf>', methods=['PUT'])
+@users_blueprint.route('/edit_user/<string:cpf>', methods=['PUT'])
 @needs_auth
 def put_user(cpf):
-    response = requests.put("%s/api/edit_user/%s" % (os.getenv('USERS_PATH'), cpf))
+    data = request.get_json()
+    response = requests.put("%s/api/edit_user/%s" % (os.getenv('USERS_PATH'), cpf), json=data)
     return jsonify(response.json()), response.status_code
 
 @users_blueprint.route('/user/list', methods=['GET'])
@@ -38,57 +40,56 @@ def auth_user():
     response = requests.post("%s/api/token/" % os.getenv('USERS_PATH'), json=data)
     return jsonify(response.json()), response.status_code
 
-cards_blueprint = Blueprint('cards', __name__)
+@users_blueprint.route('/user/post_image', methods=['POST'])
+@needs_auth
+def create_user_image():
+    data = request.get_json()
+    response = requests.post("%s/api/user/post_image" % os.getenv('USERS_PATH'), json=data)
+    return jsonify(response.json()), response.status_code
+
+@users_blueprint.route('/user/get_image/<string:cpf>', methods=['GET'])
+@needs_auth
+def get_user_image(cpf):
+    response = requests.get("%s/api/user/get_image/%s" % (os.getenv('USERS_PATH'), cpf))
+    return jsonify(response.json()), response.status_code
+
+@users_blueprint.route('/user/delete_image/<string:cpf>/', methods=['DELETE'])
+@needs_auth
+def delete_user_image(cpf):
+    response = requests.get("%s/user/delete_image/%s" % (os.getenv('USERS_PATH'), cpf))
+    return jsonify(response.json()), response.status_code
+
+cards_blueprint = Blueprint('users', __name__)
 CORS(cards_blueprint)
 
 @cards_blueprint.route('/user/card', methods=['POST'])
 def create_card():
     data = request.get_json()
-    response = requests.post("%s/api/user/card" % os.getenv('CARDS_PATH'), json=data)
+    response = requests.post("%s/api/user/card" % os.getenv('USERS_PATH'), json=data)
     return jsonify(response.json()), response.status_code
 
 @cards_blueprint.route('/user/card/<int:id>', methods=['GET'])
 @needs_auth
 def get_card(id):
-    response = requests.get("%s/api/user/card/%s" % (os.getenv('CARDS_PATH'), id))
+    response = requests.get("%s/api/user/card/%s" % (os.getenv('USERS_PATH'), id))
     return jsonify(response.json()), response.status_code
 
 @cards_blueprint.route('/user/card/<int:id>', methods=['PUT'])
 @needs_auth
 def put_card(id):
-    response = requests.put("%s/api/user/card/%s" % (os.getenv('CARDS_PATH'), id))
+    response = requests.put("%s/api/user/card/%s" % (os.getenv('USERS_PATH'), id))
     return jsonify(response.json()), response.status_code
 
 @cards_blueprint.route('/user/card/<int:id>', methods=['DELETE'])
 @needs_auth
 def delete_card(id):
-    response = requests.put("%s/api/user/card/%s" % (os.getenv('CARDS_PATH'), id))
+    response = requests.put("%s/api/user/card/%s" % (os.getenv('USERS_PATH'), id))
     return jsonify(response.json()), response.status_code
 
 @cards_blueprint.route('/user/user_cards/', methods=['GET'])
 @needs_auth
 def get_user_cards(cpf):
-    response = requests.get("%s/user/user_cards/%s" % (os.getenv('CARDS_PATH'), cpf))
+    response = requests.get("%s/user/user_cards/%s" % (os.getenv('USERS_PATH'), cpf))
     return jsonify(response.json()), response.status_code
 
 
-image_blueprint = Blueprint('images', __name__)
-CORS(image_blueprint)
-
-@image_blueprint.route('/user/image', methods=['POST'])
-def create_user_image():
-    data = request.get_json()
-    response = requests.post("%s/api/user/post_image" % os.getenv('USER_IMAGE_PATH'), json=data)
-    return jsonify(response.json()), response.status_code
-
-@image_blueprint.route('/user/image/<string:cpf>', methods=['GET'])
-@needs_auth
-def get_user_image(cpf):
-    response = requests.get("%s/api/user/get_image/%s" % (os.getenv('USER_IMAGE_PATH'), cpf))
-    return jsonify(response.json()), response.status_code
-
-@image_blueprint.route('/user/image/<string:cpf>/', methods=['DELETE'])
-@needs_auth
-def delete_user_image(cpf):
-    response = requests.get("%s/user/delete_image/%s" % (os.getenv('USER_IMAGE_PATH'), cpf))
-    return jsonify(response.json()), response.status_code
