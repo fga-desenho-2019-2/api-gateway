8022ddc47f05d2fe5ad4ef69ac3ec3470b8c96f6
Sara Silva <saracsas2@gmail.com>
2019-11-16 23:28:07 -0300

    Add urls for items and restaurant

--- project/api/restaurant/views.py
+++ project/api/restaurant/views.py
@@ -6,3 +6,38 @@ import os
 restaurant_blueprint = Blueprint('restaurant', __name__)
 CORS(restaurant_blueprint)
 
+@restaurant_blueprint.route('/api/restaurants', methods=['GET'])
+def get_all_restaurant():
+    response = requests.get('%s/api/restaurant' % os.getenv('RESTAURANT_PATH')) 
+
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return 400
+
+@restaurant_blueprint.route('/api/restaurant/<int:pk>', methods=['GET'])
+def get_one_restaurant():
+    response = requests.get('%s/api/restaurant/<int:pk>' % os.getenv('RESTAURANT_PATH')) 
+    
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return 400
+
+@restaurant_blueprint.route('/api/items', methods=['GET'])
+def get_all_items():
+    response = requests.get('%s/api/item' % os.getenv('RESTAURANT_PATH')) 
+    
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return 400
+
+@restaurant_blueprint.route('/api/item/<int:pk>', methods=['GET'])
+def get_one_item():
+    response_item = requests.get('%s/api/item/<int:pk>' % os.getenv('RESTAURANT_PATH')) 
+    
+    if response.status_code == 200:
+        return jsonify(response.json()), response.status_code
+
+    return 400
