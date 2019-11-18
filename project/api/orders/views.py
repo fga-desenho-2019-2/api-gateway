11ceaafc820a936db9477842bd5394f219fb169b
alanrslima <alanronison.lima@gmail.com>
2019-11-17 22:32:06 -0300

    #111 Adding connection with order service

--- project/api/orders/views.py
+++ project/api/orders/views.py
@@ -16,3 +16,21 @@ def order_bag():
         return jsonify(response_order.json()), response_order.status_code
 
     return 400
+
+
+@orders_blueprint.route('/api/list-user-orders/<string:cpf>/<int:status>', methods=['GET'])
+def user_orders(cpf, status):
+    response = requests.get("%s/api/list_user_orders/%s/%s" % (os.getenv('ORDERS_PATH'), cpf, status))
+    return jsonify(response.json()), response.status_code
+
+
+@orders_blueprint.route('/api/list-restaurant-orders/<string:cnpj>/<int:status>', methods=['GET'])
+def restaurant_orders(cnpj, status):
+    response = requests.get("%s/api/list_restaurant_orders/%s/%s" % (os.getenv('ORDERS_PATH'), cnpj, status))
+    return jsonify(response.json()), response.status_code
+
+
+@orders_blueprint.route('/api/update-status-order/<int:id>/<int:status>', methods=['PUT'])
+def update_status_order(id, status):
+    response = requests.put("%s/api/update_status_order/%s/%s" % (os.getenv('ORDERS_PATH'), id, status))
+    return jsonify(response.json()), response.status_code
